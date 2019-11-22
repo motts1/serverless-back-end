0ce80846e15acf493b30c8b131d2759c3d873209
Sierra <mottsiaira@gmail.com>
2019-11-22 10:30:58 -0800

    process.env -> 'notes'

--- update.js
+++ update.js
@@ -4,7 +4,7 @@ import {success, failure} from "./libs/response-lib";
 export async function main(event, context){
     const data = JSON.parse(event.body);
     const params ={
-        TableName: process.env.tableName,
+        TableName: 'notes',
         Key: {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: event.pathParameters.id
@@ -16,7 +16,7 @@ export async function main(event, context){
         },
         ReturnValues: "ALL_NEW"
     };
-    try{ 
+    try{
         await dynamoDbLib.call("update", params);
         return success({ status : true });
     }catch (e) {
