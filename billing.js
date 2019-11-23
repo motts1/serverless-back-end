70715b812f40d9cecefc53503c8521291a535df2
Sierra <mottsiaira@gmail.com>
2019-11-22 16:47:42 -0800

    update

--- billing.js
+++ billing.js
@@ -1,5 +1,5 @@
 import stripePackage from "stripe";
-import { calculateCost } from "./libs/billing-lib";
+import { calculateCost } from "./libs/billings-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
